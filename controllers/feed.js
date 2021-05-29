2b13d89db02d319653da1eabc6183b31dac4b45d
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-30 01:38:19 +0200

    clear User-Post db relations

--- controllers/feed.js
+++ controllers/feed.js
@@ -175,7 +175,13 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndRemove(postId);
     })
     .then((result) => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(() => {
       res.status(200).json({ message: "Deteled post." });
     })
     .catch((err) => {
