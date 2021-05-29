496100932fdc248734635460bf264d073fe47dc8
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-29 12:21:48 +0200

    add a create post endpoint

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post!" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/duck.jpg",
+        creator: {
+          name: "Elate",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
@@ -8,15 +19,14 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   // Create post in db
-
   res.status(201).json({
     message: "Post created successfully!",
-    posts: [
-      {
-        id: new Date().toISOString(),
-        title: title,
-        content: content,
-      },
-    ],
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Elate" },
+      createdAt: new Date(),
+    },
   });
 };
