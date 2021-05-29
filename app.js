3bf32fe34e2b3d68717675f8887d2d6dc0b2f3b0
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-29 22:19:29 +0200

    add a User model

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
 
+const authRoutes = require("./routes/auth");
 const feedRoutes = require("./routes/feed");
 
 const app = express();
@@ -49,6 +50,7 @@ app.use((req, res, next) => {
   next();
 });
 // GET /feed/posts
+app.use("/auth", autRoutes);
 app.use("/feed", feedRoutes);
 
 // error handling middleware
