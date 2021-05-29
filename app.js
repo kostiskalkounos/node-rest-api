a86e1b835bc203ef576970d57a467beee5144034
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-29 10:46:36 +0200

    REST APIs, Clients & CORS Errors

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const feedRoutes = require("./routes/feed");
 
 const app = express();
 
+// app.use(express.urlencoded()); // x-www-form-urlencoded <form>
+
+app.use(express.json()); // application/json
+
+app.use((req, res, next) => {
+  // Set the headers for all responses to avoid CORS error on client side
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 // GET /feed/posts
 app.use("/feed", feedRoutes);
 
