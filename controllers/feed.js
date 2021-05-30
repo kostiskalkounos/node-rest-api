9776ec51bfedbf9ea39ef4ca77f1a49b85ceace2
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-30 15:53:09 +0200

    send the post to all connected clients

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,7 @@ const path = require("path");
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 const User = require("../models/user");
+const io = require("../socket");
 
 exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -55,6 +56,10 @@ exports.createPost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.push(post);
     await user.save();
+    io.getIO().emit("posts", {
+      action: "create",
+      post: { ...post._doc, creator: { _id: req.userId, name: user.name } },
+    }); // the data we send can be of any format, not an object necessarily
     res.status(201).json({
       message: "Post created successfully!",
       post: post,
